30e199a3c327bd07dc0a0f48728d64f88acff471
H-Manuel <hm2799@mci4me.at>
2024-11-12 10:47:10 +0100

    LED Test main file

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,19 @@
 
 // put function declarations here:
 int myFunction(int, int);
+bool toggle=0;
+int LED_pin=6;
 
 void setup() {
   // put your setup code here, to run once:
   int result = myFunction(2, 3);
+  pinMode(LED_pin,OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(LED_pin,toggle);
+  toggle= !toggle;
+  delay(1000);
 }
 
 // put function definitions here:
