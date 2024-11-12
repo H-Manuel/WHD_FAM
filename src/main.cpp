e7488f6ada6d4a4ca0112118f99336043517aeff
H-Manuel <hm2799@mci4me.at>
2024-11-12 11:37:23 +0100

    Test LED blinken

--- src/main.cpp
+++ src/main.cpp
@@ -9,15 +9,16 @@ void setup() {
   // put your setup code here, to run once:
   int result = myFunction(2, 3);
   pinMode(LED_pin,OUTPUT);
+  Serial.begin(115200);
 }
 
 void loop() {
   digitalWrite(LED_pin,toggle);
   toggle= !toggle;
+  Serial.println("test");
   delay(1000);
 }
 
 // put function definitions here:
 int myFunction(int x, int y) {
   return x + y;
-}
